e51138aded55f63f6a678eedd92aae6dcafd2215
huangchengjie <597857074@qq.com>
2024-04-11 12:45:38 +0800

    feat: render bezierCurve

--- src/chart.ts
+++ src/chart.ts
@@ -24,7 +24,7 @@ import type {
 } from 'echarts/components'
 import type { ComposeOption } from 'echarts/core'
 import { parseGraphModelFromRawData, RawData } from './helper'
-import { Arc, CirCosModel } from './circos-model'
+import { Arc, BezierCurve, CirCosModel, InnerArc, OutterArc } from './circos-model'
 import { CustomRootElementOption } from 'echarts/types/src/chart/custom/CustomSeries.js'
 
 // 通过 ComposeOption 来组合出一个只有必须组件和图表的 Option 类型
@@ -76,27 +76,27 @@ export function initChart(root: HTMLDivElement) {
     series: [
       ...circosModel.outterArcs.map((arc) => (getArcSeriesOption(arc))),
       ...circosModel.innerArcs.map((arc) => (getArcSeriesOption(arc))),
+      ...circosModel.innerBezierCurves.map((bezierCurve) => (getBezierCurveSeriesOption(bezierCurve))),
     ],
   }
 
-  function getArcSeriesOption(arc: Arc) {
+  function getArcSeriesOption(arc: OutterArc | InnerArc) {
     const arcSeriesOption: CustomSeriesOption = {
       // 使用自定义系列
       type: 'custom',
       coordinateSystem: 'none',
       name: arc.node.id,
       renderItem: (params, api) => {
-        console.log(api.visual('color', 3))
         const width = api.getWidth()
         const height = api.getHeight()
-        const size = Math.min(width, height)
+        const size = Math.min(width, height) / 2
         const arcEle: CustomRootElementOption = {
           type: 'sector',
           shape: {
-            cx: width / 2,
-            cy: height / 2,
-            r0: (arc.shape.r - 0.05) * size / 2,
-            r: arc.shape.r * size / 2,
+            cx: arc.shape.cx * size,
+            cy: arc.shape.cy * size,
+            r0: (arc.shape.r - 0.05) * size,
+            r: arc.shape.r * size,
             startAngle: arc.shape.start * Math.PI * 2,
             endAngle: arc.shape.end * Math.PI * 2,
           },
@@ -107,6 +107,8 @@ export function initChart(root: HTMLDivElement) {
 
         const groupEle: CustomRootElementOption = {
           type: 'group',
+          x: width / 2,
+          y: height / 2,
           children: [arcEle],
         }
         return groupEle as any
@@ -120,3 +122,41 @@ export function initChart(root: HTMLDivElement) {
   return chart
 }
 
+function getBezierCurveSeriesOption(bezierCurve: BezierCurve) {
+  const bezierCurveSeriesOption: CustomSeriesOption = {
+    type: 'custom',
+    coordinateSystem: 'none',
+    name: bezierCurve.edge.source.id,
+    renderItem: (params, api) => {
+      const width = api.getWidth()
+      const height = api.getHeight()
+      const size = Math.min(width, height) / 2
+      const bezierCurveEle: CustomRootElementOption = {
+        type: 'bezierCurve',
+        shape: {
+          x1: bezierCurve.shape.x1 * size,
+          y1: bezierCurve.shape.y1 * size,
+          x2: bezierCurve.shape.x2 * size,
+          y2: bezierCurve.shape.y2 * size,
+          cpx1: bezierCurve.shape.cpx1 * size,
+          cpy1: bezierCurve.shape.cpy1 * size,
+          cpx2: bezierCurve.shape.cpx2 * size,
+          cpy2: bezierCurve.shape.cpy2 * size,
+        },
+        style: {
+          stroke: api.visual('color'),
+        },
+      }
+
+      const groupEle: CustomRootElementOption = {
+        type: 'group',
+        x: width / 2,
+        y: height / 2,
+        children: [bezierCurveEle],
+      }
+      return groupEle as any
+    },
+  }
+
+  return bezierCurveSeriesOption
+}
--- src/circos-model.ts
+++ src/circos-model.ts
@@ -1,4 +1,5 @@
-import { Graph, Node, GraphModel } from "./graph"
+import { Graph, Node, Edge, GraphModel } from "./graph"
+import { assert } from "./utils"
 
 
 export interface ArcShape {
@@ -22,18 +23,27 @@ export interface BezierCurveShape {
 }
 
 export interface Arc {
-  node: Node
   shape: ArcShape
 }
 
+export interface OutterArc extends Arc {
+  node: Node
+}
+
+export interface InnerArc extends Arc {
+  node: Node
+  parent: OutterArc
+}
+
 export interface BezierCurve {
+  edge: Edge
   shape: BezierCurveShape
 }
 
 export class CirCosModel {
   graph: Graph
-  outterArcs: Arc[]
-  innerArcs: Arc[]
+  outterArcs: OutterArc[]
+  innerArcs: InnerArc[]
   innerBezierCurves: BezierCurve[]
 
   get model() {
@@ -43,8 +53,8 @@ export class CirCosModel {
   constructor(graphModel: GraphModel) {
     this.graph = this.initGraph(graphModel)
     this.outterArcs = this.initOutterArcs(this.graph)
-    this.innerArcs = this.initInnerArcs(this.graph)
-    this.innerBezierCurves = this.initInnerBezierCurves(this.graph)
+    this.innerArcs = this.initInnerArcs(this.graph, this.outterArcs)
+    this.innerBezierCurves = this.initInnerBezierCurves(this.graph, this.innerArcs)
   }
 
   initGraph(graphModel: GraphModel) {
@@ -52,7 +62,7 @@ export class CirCosModel {
     return graph
   }
 
-  initOutterArcs(graph: Graph) {
+  initOutterArcs(graph: Graph): OutterArc[] {
     // Sum of all nodes' value must be 1
     const GAP = 0.01
     const totalGapSize = GAP * graph.nodes.length
@@ -60,7 +70,7 @@ export class CirCosModel {
     let start = 0
     const arcs = graph.nodes.map(node => {
       const end = start + node.value * scale
-      const arc: Arc = ({
+      const arc: OutterArc = ({
         node,
         shape: {
           start: start,
@@ -76,17 +86,20 @@ export class CirCosModel {
     return arcs
   }
 
-  initInnerArcs(graph: Graph) {
+  initInnerArcs(graph: Graph, outterArcs: OutterArc[]): InnerArc[] {
     const GAP = 0.015
     const totalGapSize = GAP * graph.nodes.length
     const scale = 1 - totalGapSize
     let start = 0
     const arcGroups = graph.nodes.map(node => {
       let end = 0
+      const outterArc = outterArcs.find(arc => arc.node === node)
+      assert(!!outterArc, `outterArc not found for node ${node.id}`)
       const group = node.edges.map(edge => {
         end = start + edge.value / 2 * scale
-        const arc: Arc = {
-          node: node === edge.source ? edge.target : edge.source,
+        const arc: InnerArc = {
+          parent: outterArc,
+          node: edge.source === node ? edge.target : edge.source,
           shape: {
             start: start,
             end: end,
@@ -104,8 +117,45 @@ export class CirCosModel {
     return arcGroups.flat()
   }
 
-  initInnerBezierCurves(graph: Graph) {
-    // TODO
-    return []
+  initInnerBezierCurves(graph: Graph, innerArcs: InnerArc[]) {
+    function findArcByEdge(source: Node, target: Node) {
+      const arcs = innerArcs.filter(arc => arc.parent.node === source)
+      const arc = arcs.find(arc => arc.node === target)
+      assert(!!arc, `arc not found for edge ${source.id} -> ${target.id}`)
+      return arc
+    }
+
+    function getMiddlePointOfArc(arc: Arc) {
+      const { start, end, cx, cy, r } = arc.shape
+      const middleAngle = (start + end) / 2 * Math.PI * 2
+      const x = cx + r * Math.cos(middleAngle)
+      const y = cy + r * Math.sin(middleAngle)
+      return { x, y }
+    }
+
+    const curves = graph.edges.map<BezierCurve>(edge => {
+      const startArc = findArcByEdge(edge.source, edge.target)
+      const endArc = findArcByEdge(edge.target, edge.source)
+      const startArcMiddle = getMiddlePointOfArc(startArc)
+      const endArcMiddle = getMiddlePointOfArc(endArc)
+      return {
+        edge,
+        shape: {
+          // middle point of the source arc
+          x1: startArcMiddle.x,
+          y1: startArcMiddle.y,
+          x2: endArcMiddle.x,
+          y2: endArcMiddle.y,
+          // control point of the source arc
+          cpx2: startArcMiddle.x,
+          cpy2: startArcMiddle.y,
+          // control point of the target arc
+          cpx1: endArcMiddle.x,
+          cpy1: endArcMiddle.y,
+        }
+      }
+    })
+
+    return curves
   }
 }
